515a91d469eaf91242bf7f8c83a83262cdd1add2
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-05-17 20:29:08 -0300

    refactor: add CORS configuration and change base url user routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Services\UsersService;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-Route::middleware('api')->prefix('users')->group(function () {
-    Route::get('/', function (Request $request, UsersService $userService) {
-        return $userService->getAllUsers($request);
-    });
-    Route::post('/', function (Request $request, UsersService $userService) {
-        return $userService->postUser($request);
-    });
-});
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();

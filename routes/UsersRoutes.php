acb71a22335a4b6fbffb94574ff60dd609051e30
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-05-29 20:44:28 -0300

    feat: finalizing auth middleware

--- routes/UsersRoutes.php
+++ routes/UsersRoutes.php
@@ -5,13 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Services\UsersService;
 
 Route::prefix('users')->group(function () {
-    Route::get('/', function (Request $request, UsersService $userService) {
-        return $userService->getAllUsers($request);
-    });
-    Route::get('/{id}', function (string $id, UsersService $userService) {
-        return $userService->getUser($id);
-    });
-    Route::delete('/{id}', function (string $id, UsersService $userService) {
-        return $userService->deleteUser($id);
+    Route::middleware(['auth:api'])->group(function () {
+        Route::get('/', function (Request $request, UsersService $userService) {
+            return $userService->getAllUsers($request);
+        });
+        Route::get('/{id}', function (string $id, UsersService $userService) {
+            return $userService->getUser($id);
+        });
+        Route::delete('/{id}', function (string $id, UsersService $userService) {
+            return $userService->deleteUser($id);
+        });
     });
 });

48f8a067ff69c41c89e8fa6abe62e5f04df08946
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-05-29 20:54:05 -0300

    feat: adding global rate limiter by ip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Cache\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        app(RateLimiter::class)->for('global', function ($request) {
+            return Limit::perMinute(100)->by($request->ip());
+        });
+
+        $middleware->append(\Illuminate\Routing\Middleware\ThrottleRequests::class . ':global');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

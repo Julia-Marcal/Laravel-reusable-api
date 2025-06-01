c9daa1c77731f02019e213441f19393447b34020
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-06-01 17:03:05 -0300

    fix: cache.store bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Cache\RateLimiter;
-use Illuminate\Cache\RateLimiting\Limit;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,11 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        app(RateLimiter::class)->for('global', function ($request) {
-            return Limit::perMinute(100)->by($request->ip());
-        });
-
-        $middleware->append(\Illuminate\Routing\Middleware\ThrottleRequests::class . ':global');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
